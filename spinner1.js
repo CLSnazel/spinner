243833c717606dd628381cda78e606346210b9e5
Chantal Snazel <clsnazel@gmail.com>
2020-09-22 15:57:29 +0000

    completed first spinner activity

---
+++ spinner1.js
@@ -0,0 +1,26 @@
+
+const spinner1 = function() {
+  setTimeout(() => {
+    process.stdout.write(`\r|   `);
+  }, 100);
+  setTimeout(() => {
+    process.stdout.write(`\r/   `);
+  }, 300);
+  setTimeout(() => {
+    process.stdout.write(`\r-   `);
+  }, 500);
+  setTimeout(() => {
+    process.stdout.write(`\r\\    `);
+  }, 700);
+};
+
+for(let i = 0; i < 3; i++){
+  setTimeout(() => {
+    spinner1();
+  }, 800 * i);
+}
+
+setTimeout(() => {
+  console.log('');
+}, 800 * 3 + 100);
+
